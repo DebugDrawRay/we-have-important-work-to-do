370b278f49919e6d75342d95309a909a5997c4d4
Raymond John Grice <raymondjgrice@gmail.com>
2018-08-10 21:03:13 -0700

    Multi window drifting

---
+++ Assets/InterfaceManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InterfaceManager : MonoBehaviour
+{
+    [SerializeField] private RectTransform m_minimizedContainer;
+
+    public static InterfaceManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public void AddToMinimized(RectTransform item)
+    {
+        item.SetParent(m_minimizedContainer);
+    }
+}
--- Assets/Scripts/WindowController.cs
+++ Assets/Scripts/WindowController.cs
@@ -6,25 +6,38 @@ namespace FSS
 {
 	public class WindowController : MonoBehaviour 
 	{
+        [SerializeField] private GameObject m_minimizeIcon;
+        private Button miniIcon;
 		private RectTransform m_rt;
         private Vector3 m_cursorOffset;
 
 		private void Awake()
 		{
 			m_rt = GetComponent<RectTransform>();
+            miniIcon = Instantiate(m_minimizeIcon).GetComponent<Button>();
+            miniIcon.onClick.AddListener(Enlarge);
+            InterfaceManager.instance.AddToMinimized(miniIcon.GetComponent<RectTransform>());
 		}
 
 		public void Minimize()
 		{
+            gameObject.SetActive(false);
+            m_minimizeIcon.SetActive(true);
+        }
 
-		}
-		public void Close()
+        public void Enlarge()
+        {
+            gameObject.SetActive(true);
+        }
+
+        public void Close()
 		{
-
+            gameObject.SetActive(false);
 		}
         public void BeginDrag()
         {
             m_cursorOffset = transform.position - Input.mousePosition;
+            transform.SetAsLastSibling();
         }
 		public void Drag()
 		{
