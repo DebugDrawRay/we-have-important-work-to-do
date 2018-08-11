393e4379c37df6a3d8024e4dd259512226627347
Raymond John Grice <raymondjgrice@gmail.com>
2018-08-10 20:27:03 -0700

    Added draggable windows

---
+++ Assets/Scripts/WindowController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+namespace FSS
+{
+	public class WindowController : MonoBehaviour 
+	{
+		private RectTransform m_rt;
+        private Vector3 m_cursorOffset;
+
+		private void Awake()
+		{
+			m_rt = GetComponent<RectTransform>();
+		}
+
+		public void Minimize()
+		{
+
+		}
+		public void Close()
+		{
+
+		}
+        public void BeginDrag()
+        {
+            m_cursorOffset = transform.position - Input.mousePosition;
+        }
+		public void Drag()
+		{
+            transform.position = m_cursorOffset + Input.mousePosition;
+        }
+	}
+}
