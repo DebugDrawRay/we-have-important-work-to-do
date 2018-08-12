c216789648e6fb38eaf8271be54a605ebf1f6c60
Raymond John Grice <raymondjgrice@gmail.com>
2018-08-11 17:49:54 -0700

    Intro sequence complete

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -28,16 +28,50 @@ namespace FSS
         [Header("Currency")]
         [SerializeField] private SuperTextMesh m_currencyText;
         private int m_currentCurrency;
-
+        
         [Header("Time")]
         [SerializeField] private SuperTextMesh m_clock;
         private DateTime m_currentClockTime;
         private float m_gameStartTime;
         private float m_lastClockUpdate;
         private float m_totalMinutes;
-        public delegate void WindowCallback(WindowController windows);
 
+        [Header("Management")]
+        [SerializeField] private TypewriterEffect m_welcomePrompt;
+        [SerializeField] private GameObject m_managementGroup;
+        [SerializeField] private GameObject m_loginScreen;
+        [SerializeField] private GameObject m_sequenceScreen;
+        [SerializeField] private float m_stateDelay = 3f;
+        private string m_playerName;
+        private GameState m_currentState;
+        public enum GameState
+        {
+            Boot,
+            Login,
+            Sequence,
+            InGame,
+            Complete
+        }
+        public delegate void WindowCallback(WindowController windows);
         private void Awake()
+        {
+            m_managementGroup.SetActive(false);
+            m_loginScreen.SetActive(false);
+            m_sequenceScreen.SetActive(false);
+            TransitionToNextState((int)GameState.Login);
+        }
+
+        private void Update()
+        {
+            if(m_currentState == GameState.InGame)
+            {
+                UpdatePopUps();
+                UpdateRam();
+                UpdateTime();
+            }
+        }
+
+        public void SetupGame()
         {
             m_currentRam = Settings.StartRam;
 
@@ -50,13 +84,6 @@ namespace FSS
             m_totalMinutes = (Settings.EasyClockEnd.ToUniversalTime().Hour - Settings.StartClockTime.ToUniversalTime().Hour) * 60;
 
             m_gameStartTime = Time.time;
-        }
-
-        private void Update()
-        {
-            UpdatePopUps();
-            UpdateRam();
-            UpdateTime();
         }
 
         private void UpdateRam()
@@ -115,6 +142,63 @@ namespace FSS
                 float progress = (elapsed / Settings.EasyModeTime) * m_totalMinutes;
                 m_currentClockTime = Settings.StartClockTime.AddMinutes(Mathf.RoundToInt(progress));
                 m_clock.text = m_currentClockTime.ToShortTimeString();
+            }
+        }
+
+        public void UpdateName(string name)
+        {
+            m_playerName = name;
+            m_welcomePrompt.m_textToWrite = "Good morning " + name +".";
+        }
+
+        public void TransitionToNextState(int state)
+        {
+            ExitState();
+            m_currentState = (GameState)state;
+            StartCoroutine(StateDelay(m_stateDelay));
+        }
+        public void ExitState()
+        {
+            switch (m_currentState)
+            {
+                case GameState.Boot:
+                    m_managementGroup.SetActive(true);
+                    break;
+                case GameState.Login:
+                    m_loginScreen.SetActive(false);
+                    break;
+                case GameState.Sequence:
+                    m_sequenceScreen.SetActive(false);
+                    m_managementGroup.SetActive(false);
+                    break;
+                case GameState.InGame:
+                    break;
+                case GameState.Complete:
+                    break;
+            }
+        }
+        public IEnumerator StateDelay(float time)
+        {
+            yield return new WaitForSeconds(time);
+            EnterState();
+        }
+        public void EnterState()
+        {
+            switch(m_currentState)
+            {
+                case GameState.Boot:
+                    break;
+                case GameState.Login:
+                    m_loginScreen.SetActive(true);
+                    break;
+                case GameState.Sequence:
+                    m_sequenceScreen.SetActive(true);
+                    m_welcomePrompt.Write();
+                    break;
+                case GameState.InGame:
+                    break;
+                case GameState.Complete:
+                    break;
             }
         }
     }
---
+++ Assets/TypewriterEffect.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using DG.Tweening;
+using UnityEngine.UI;
+namespace FSS
+{
+    public class TypewriterEffect : MonoBehaviour
+    {
+        public string m_textToWrite;
+        [SerializeField] private float m_writeSpeed;
+        [SerializeField] private float m_eventDelay;
+        [SerializeField] private bool m_storeOriginalText = true;
+        [SerializeField] private bool m_playOnStart = false;
+        [SerializeField] private UnityEvent m_onComplete;
+
+        private string m_originalText;
+        private Text text;
+        private Tween type;
+        private void Awake()
+        {
+            text = GetComponent<Text>();
+            m_originalText = text.text;
+        }
+        private void Start()
+        {
+            if (m_playOnStart)
+            {
+                Write();
+            }
+        }
+
+        public void Write()
+        {
+            if (type != null && type.IsActive())
+            {
+                type.Kill();
+            }
+            text.text = "";
+            type = text.DOText(m_textToWrite, m_writeSpeed).OnComplete(InvokeOnComplete).SetEase(Ease.Linear);
+        }
+        public void Revert(bool typewrite)
+        {
+            if (type != null && type.IsActive())
+            {
+                type.Kill();
+            }
+            text.text = "";
+            if (typewrite)
+            {
+                text.DOText(m_originalText, m_writeSpeed).SetEase(Ease.Linear);
+            }
+            else
+            {
+                text.text = m_originalText;
+            }
+        }
+
+        private void InvokeOnComplete()
+        {
+            StartCoroutine(InvokeCoroutine());
+        }
+
+        private IEnumerator InvokeCoroutine()
+        {
+            yield return new WaitForSeconds(m_eventDelay);
+            m_onComplete.Invoke();
+        }
+    }
+}
