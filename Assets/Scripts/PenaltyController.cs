8e6a9cffb44cb8d5dbe13805196c0f400409d81e
Raymond John Grice <raymondjgrice@gmail.com>
2018-08-12 01:40:10 -0700

    More Ads

--- Assets/Scripts/PenaltyController.cs
+++ Assets/Scripts/PenaltyController.cs
@@ -2,15 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PenaltyController : MonoBehaviour {
+public class PenaltyController : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
