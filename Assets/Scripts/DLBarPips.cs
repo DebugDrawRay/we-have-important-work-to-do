1c883b1574833c7d928dce12c4006c6474ba6fa1
breadotop <33920871+breadotop@users.noreply.github.com>
2018-08-13 09:24:15 -0700

    download virus window complete

---
+++ Assets/Scripts/DLBarPips.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DLBarPips : MonoBehaviour {
+
+    [SerializeField] private GameObject[] dlPips;
+    [SerializeField] private float downloadTime;
+    private float startTime;
+    private float elapsedTime;
+    private float elapsedPercentage;
+
+    private void Awake () {
+        startTime = Time.time;
+	}
+	
+	private void Update () {
+        elapsedTime = Time.time - startTime;
+        elapsedPercentage = elapsedTime / downloadTime;
+        UpdateDL();
+	}
+
+    private void UpdateDL()
+    {
+        float progress = elapsedPercentage * dlPips.Length;
+
+        for (int i = 1; i < dlPips.Length; i++)
+        {
+            dlPips[i].SetActive(i + 1 <= progress);
+        }
+
+    }
+
+}
